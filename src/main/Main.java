20c563af0b440393e669ebc628615f09fa9e3184
Kenny Wibowo <KennyWibowo@users.noreply.github.com>
2014-09-23 12:17:27 -0700

    Fixed comments of main,java

--- src/main/Main.java
+++ src/main/Main.java
@@ -33,28 +33,28 @@ public class Main {
 		Fibonacchi.run();			//problem 2
 		PrimeFactor.run();			//problem 3
 		Palindrome.run();			//problem 4
-		SmallestMultiple.run();		//problem 5
-		SumsAndSquares.run();		//problem 6
+		SmallestMultiple.run();			//problem 5
+		SumsAndSquares.run();			//problem 6
 		MorePrimes.run();			//problem 7
-		ProductSeries.run();		//problem 8
+		ProductSeries.run();			//problem 8
 		PythagTriple.run();			//problem 9
 		PrimeSum.run();				//problem 10
 		GridProduct.run();			//problem 11
 		TriangNum.run();			//problem 12
 		LargeSum.run();				//problem 13
-		PowerDigitSum.run();		//problem 16
-//		NumberLetters.run();		//problem 17
+		PowerDigitSum.run();			//problem 16
+//		NumberLetters.run();			//problem 17
 		PathSum.run();				//problem 18
-		CountingSundays.run();		//problem 19
-		FactorialDigitSum.run();	//problem 20
-		AmicableNumbers.run();		//problem 21
+		CountingSundays.run();			//problem 19
+		FactorialDigitSum.run();		//problem 20
+		AmicableNumbers.run();			//problem 21
 		NameScores.run();			//problem 22
-		NonAbundantSums.run();		//problem 23
+		NonAbundantSums.run();			//problem 23
 		PathSumLarge.run();			//problem 67
 		long endTime = System.currentTimeMillis();        //Time end
 		out.println("\n" + (endTime-startTime) + " ms taken to complete calculations.");
 	}
-	private static void timer(long start, long finish){ //Deprecated, takes in different nanoTime() readings and prints time in between in seconds.
+	private static void timer(long start, long finish){ //Deprecated, takes in different System.nanoTime() readings and prints time taken in seconds.
 		long timeTaken = finish - start;
 		String timeFormatted = "0.";
 		int count = String.valueOf(timeTaken).length();

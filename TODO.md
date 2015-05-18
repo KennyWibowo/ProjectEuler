f99a93bbdd138d5f46f57e7ec82c02fdb6705120
KennyWibowo <kewibowo@ucsd.edu>
2015-05-18 14:27:49 -0700

    Started work on 24

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 ## TO DO LIST ##
 
+* Finish problem 24
 * Finish problem 26
 * Make more progress :)
 * Finish to do list
--- src/pset/Twentyfour.java
+++ src/pset/Twentyfour.java
@@ -1,8 +1,29 @@
 package pset;
-import static java.lang.System.out;
+import java.util.*;
 
 public class Twentyfour {
-	public static void run(){
-		out.println(/**answer*/);
+	
+	private static final int NUM_PROBLEM = 24;
+	private static final int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	public static void run() {
+        ArrayList<Integer> perms = 
+		System.out.println("Problem " + NUM_PROBLEM + ": " + perms.get(999999));
 	}
+
+    // TODO
+    private static ArrayList<Integer> createLPerm (int[] digits, int ) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int upperLim = factorial( digits.length );
+
+        while(
+    }
+
+    private static int factorial( int input ) {
+        if( input < 2 )
+            return 1;
+        
+        return input * factorial( input - 1 );
+    }
+	
 }
